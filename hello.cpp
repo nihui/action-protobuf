6a9b71d746563d1150b327d74c608e842dac8991
nihui <shuizhuyuanluo@126.com>
2022-06-04 18:52:04 +0800

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,13 +1,10 @@
-#include <unistd.h>
 #include <stdio.h>
 
 int main()
 {
-    #pragma omp parallel for num_threads(4)
     for (int i = 0; i < 10; i++)
     {
         fprintf(stderr, "qaq %d\n", i);
-        sleep(1);
     }
 
     return 0;
